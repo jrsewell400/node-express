ba3befaa5cca3dd83284d0aace355c6319b95fc5
Jordan Sewell <jrsewell400@gmail.com>
2020-06-27 17:02:34 -0600

    Add work

--- index.js
+++ index.js
@@ -11,10 +11,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Publications';
 
+
+// gets root page
 app.get('/', (request, response) => {
   response.send('Hello, Publications');
 });
 
+// gets all papers
+app.get('/api/v1/papers', (request, response) => {
+  database('papers').select()
+    .then((papers) => {
+      response.status(200).json(papers);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+// gets all footnotes
+app.get('/api/v1/footnotes', (request, response) => {
+  database('footnotes').select()
+    .then((footnotes) => {
+      response.status(200).json(footnotes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    })
+})
+
+// adds new paper to database
+app.post('/api/v1/papers', (request, response) => {
+  const paper = request.body;
+
+  for (let requiredParameter of ['title', 'author']) {
+    if (!paper[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('papers').insert(paper, 'id')
+    .then(paper => {
+      response.status(201).json({ id: paper[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+// starts server and begins listening
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
